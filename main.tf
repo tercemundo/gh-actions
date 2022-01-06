fbb0f3c66467778b3bdb1797224988b8d5bf596b
mguazzardo <mguazzardo76@gmail.com>
2022-01-05 23:30:27 -0300

    Update main.tf
    cae el sol

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ provider "aws" {
 
 # Build the VPC
 resource "aws_vpc" "vpc" {
-  cidr_block           = "10.1.0.1/16"
+  cidr_block           = "10.3.0.0/16"
   instance_tenancy     = "default"
 
   tags = {
